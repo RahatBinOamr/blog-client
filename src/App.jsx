827ad2b866a17a9a6f5d008c8a871593789cda46
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2023-08-17 20:52:12 +0600

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './components/Page/Home';
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem('TOKEN');
-    if (!token) {
-      navigate('/login');
-    }
-  }, []);
   return (
     <div className="min-h-screen">
       <Home />
--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Blogs from '../Blog/Blog/Blogs';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('TOKEN');
+    if (!token) {
+      navigate('/login');
+    }
+  }, []);
   return (
     <div>
       <Blogs />
